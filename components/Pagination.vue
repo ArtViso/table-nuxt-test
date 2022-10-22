36499e3b05928bce2f19ddc57ea7ac457e8a8026
VISO_VA <off.club1515@gmail.com>
2022-10-22 10:55:02 +0300

    added fix

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -1,13 +1,52 @@
 <template>
-$END$
+  <div class="pagination-container">
+    <div class="pagination">
+      <div v-for="(page, index) in pageNumbers" :key="index">
+        <button
+          v-if="index > 0"
+          class="pagination-button"
+          @click="changePage(index - 1)"
+        >
+          {{ index }}
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-name: "Pagination"
+  name: "Pagination",
+  props: ['pageNumbers'],
+  methods: {
+    changePage(index) {
+      this.$emit("changePage", index)
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.pagination {
+  display: flex;
+  max-height: 100px;
 
+  &-container {
+    display: grid;
+    width: 100%;
+  }
+
+  &-button {
+    padding: 10px;
+    place-items: center;
+    border-radius: 10px;
+    position: relative;
+    left: 530px;
+    background: rgb(161, 160, 160);
+    cursor: pointer;
+    &:hover {
+      background: crimson;
+    }
+  }
+}
 </style>
